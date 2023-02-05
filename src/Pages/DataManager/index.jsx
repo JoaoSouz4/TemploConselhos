8713fb65adcb0ee0d7631a8ad583ccdbc002c15f
João Souza <joaosouz8@gmail.com>
2023-02-04 21:09:06 -0300

    coleta de dados

--- src/Pages/DataManager/index.jsx
+++ src/Pages/DataManager/index.jsx
@@ -3,17 +3,23 @@ import './styles.css'
 
 export const DataBaseManager = () => {
 
-    const [dataStorage, setDataStorage] = useState();
+    const [dataStorage, setDataStorage] = useState([]);
 
     useEffect(()=>{
-        const data = localStorage.getItem('key');
-        setDataStorage(data);
-        console.log(dataStorage)
-    }, [])
+        const array = []
+        if(dataStorage.length!= 0) return;
+        else {
+            console.log('ENTREI NO LOOP')
+            for(let i = 0; i<300; i++){
+                const data = localStorage.getItem(i);
+                if(data) array.push(data);
+            }
+            setDataStorage(array)
+        }   
+    },[])
 
     return (
         <div className='manager-container'>
-
             <div className="titles">
                 <h1>Genrenciador de Mensagens</h1>
                 <p>Aqui estão salvas suas mensagens favoritas</p>
@@ -21,6 +27,7 @@ export const DataBaseManager = () => {
            
                 <input type="text" placeholder='Pesquisar mensagem salva'/>
                 <div className='container-msg'>
+                    {dataStorage[3]}
                 </div>
         </div>
     )
